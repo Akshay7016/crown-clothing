4b04329e9e5ef05dcf493edd0e893c1bc3cbe9d3
Akshay7016 <asajgule@gmail.com>
2022-08-02 13:14:25 +0530

    Shop component added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import Navigation from './routes/Navigation/Navigation';
 import Home from './routes/Home/Home';
 import Authentication from './routes/Authentication/Authentication';
-
-const Shop = () => {
-    return (
-        <h1>I am shop page</h1>
-    )
-}
+import Shop from './routes/Shop/Shop';
 
 const App = () => {
     return (
