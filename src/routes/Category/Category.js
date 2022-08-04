f87a45c92720a938fee4cf24ab176b3e3ac112ce
Akshay7016 <asajgule@gmail.com>
2022-08-04 12:56:51 +0530

    Routes added inside Shop.CategoryItem component renamed with DirectoryItem. title added inside Categoy component. CategoriesPreview component added inside routes folder which renders CategoryPreview component from components folder

--- src/routes/Category/Category.js
+++ src/routes/Category/Category.js
@@ -1,4 +1,4 @@
-import { useContext, useState, useEffect } from 'react'
+import { useContext, useState, useEffect, Fragment } from 'react'
 import { useParams } from 'react-router-dom'
 
 import { CategoriesContext } from '../../context/CategoriesContext'
@@ -16,15 +16,18 @@ const Category = () => {
     }, [category, categoriesMap])
 
     return (
-        <div className='categories-container'>
-            {
-                products && products.map((product) => {
-                    return (
-                        <ProductCard key={product.id} product={product} />
-                    )
-                })
-            }
-        </div>
+        <Fragment>
+            <h2 className='category-title'>{category}</h2>
+            <div className='category-container'>
+                {
+                    products && products.map((product) => {
+                        return (
+                            <ProductCard key={product.id} product={product} />
+                        )
+                    })
+                }
+            </div>
+        </Fragment>
     )
 }
 
