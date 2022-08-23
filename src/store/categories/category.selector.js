87a9eb6170a01560f1bd0b9409ac8703019e7a85
Akshay7016 <asajgule@gmail.com>
2022-08-23 11:40:00 +0530

    reselect library added for memoization

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,11 +1,28 @@
-// convert our categories array that we store inside of Redux into the categoriesMap(Object) 
-// that we use in order to access our categories quickly and efficiently in Category and
-// CategoriesPreview component
+import { createSelector } from "reselect";
 
-export const selectCategoriesMap = (state) => {
-    return state.categories.categories.reduce((acc, category) => {
+// to get categories reducer value
+const selectCategoryReducer = (state) => state.categories;
+
+// Check whether categories reducer value changed or not
+const selectCategories = createSelector(
+    [selectCategoryReducer],
+    (categoriesSlice) => categoriesSlice.categories
+)
+
+// Check whether categories value changed or not
+export const selectCategoriesMap = createSelector(
+    [selectCategories],
+    (categories) => categories.reduce((acc, category) => {
         const { title, items } = category;
         acc[title.toLowerCase()] = items;
         return acc;
-    }, {});
-}
+    }, {})
+
+)
+
+// convert our categories array that we store inside of Redux into the categoriesMap(Object)
+// that we use in order to access our categories quickly and efficiently in Category and
+// CategoriesPreview component
+
+// Memoized => Memorization is the process in which you cache the previous value of something
+// so that if the input has not changed, then you just return back the same output.
