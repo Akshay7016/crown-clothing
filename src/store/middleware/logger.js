cfd5c29d82921b7579bea3acf9b049b5ecee5244
Akshay7016 <asajgule@gmail.com>
2022-08-23 18:00:29 +0530

    modified middleware and added redux devtools

---
+++ src/store/middleware/logger.js
@@ -0,0 +1,15 @@
+const loggerMiddleware = (store) => (next) => (action) => {
+    // If there is no action type
+    if (!action.type) {
+        return next(action);
+    }
+
+    console.log("type: " + action.type);
+    console.log("payload: " + action.payload);
+    console.log("current state: " + store.getState());
+
+    next(action);
+
+    console.log("next state: " + store.getState());
+
+}
